67b87239fcb6b425f986e73ee4fddad44fe18d8c
David Robert-Ansart <david.robertansart@gmail.com>
2019-12-19 15:33:20 +0100

    ci update 7

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -64,7 +64,7 @@ async function main() {
     }
 
     /* --- Start server --- */
-    const port = normalizePort('5000');
+    const port = normalizePort(process.env.PORT || '5000');
     const server = app.listen(port);
     server.on('error', onError);
     server.on('listening', () => {
