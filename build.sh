d29d5fa7c34bc22429e263d1d633425982c65de0
David Robert-Ansart <david.robertansart@gmail.com>
2019-12-19 16:33:16 +0100

    update sh build 2

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+echo "Building frontend"
 cd ./frontend && npm install && echo REACT_APP_BASE_APP=/back >> .env.production && npm run build
 cd ..
 rm -rf ./backend/dist/public
 mkdir ./backend/dist
 mkdir ./backend/dist/public
 mv ./frontend/build ./backend/dist/public
-cd ./backend && npm install && npm install -g typescript@1.0 && tsc
+echo "Installing backend dependencies"
+cd ./backend && npm install && npm install -g typescript@1.0
+echo "Compiling typescript"
+tsc
 cd ..
