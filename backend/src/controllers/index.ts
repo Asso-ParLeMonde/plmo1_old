5440d9ce45be3a9a47d4c1901bc9e3674c7d3476
David Robert-Ansart <david.robertansart@gmail.com>
2020-03-11 10:28:27 +0100

    Revert "User & School backend"
    This reverts commit 7b3cb8a2e883ab5fcd90c5843cd02155e7a5578e.

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -4,5 +4,3 @@ export { ThemeScenariosController } from "./themeScenario";
 export { ScenarioQuestionController } from "./scenarioQuestions";
 export { QuestionController } from "./question";
 export { LanguageController } from "./language";
-export { UserController } from "./user";
-export { SchoolController } from "./school";
