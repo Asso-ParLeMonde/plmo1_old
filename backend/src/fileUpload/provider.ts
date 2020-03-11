5b84e14d1c78529e154c6b1be8e9d854901e44ad
David Robert-Ansart <david.robertansart@gmail.com>
2020-03-11 18:18:47 +0100

    :bug: Fix firebase single file hosting

--- backend/src/fileUpload/provider.ts
+++ backend/src/fileUpload/provider.ts
@@ -1,4 +1,4 @@
 export abstract class Provider {
-  public abstract async uploadImage(filename: string, filePath: string): Promise<string>;
+  public abstract async uploadImage(filename: string, filePath: string, hasMultipleSizes?: boolean): Promise<string>;
   public abstract async deleteImage(filename: string, filePath: string): Promise<void>;
 }
