4ec63f4a1de9b63a73f7f5eea63ce02edb5e6742
David Robert-Ansart <david.robertansart@gmail.com>
2020-03-18 14:29:59 +0100

    :sparkles: Add reset password frontend logic

--- frontend/src/User/ResetPassword/index.js
+++ frontend/src/User/ResetPassword/index.js
@@ -2,18 +2,39 @@ import React, {useState} from "react";
 import PropTypes from "prop-types";
 import {withRouter} from "react-router";
 import {Button, Link, TextField, Typography} from "@material-ui/core";
+import {axiosRequest} from "../../components/axiosRequest";
+
+const errorMessages = {
+  0: "Une erreur inconnue est survenue. Veuillez réessayer plus tard...",
+  1: "E-mail invalide",
+};
 
 function ResetPassword(props) {
-  const [user, setUser] = useState({
-    username: "",
-  });
+  const [email, setEmail] = useState("");
+  const [errorCode, setErrorCode] = useState(-1);
+  const [successMsg, setSuccessMsg] = useState("");
 
   const handleUserNameInputChange = event => {
-    setUser({ ...user, username: event.target.value });
+    setEmail(event.target.value);
+    setErrorCode(-1);
   };
 
-  const submit = () => {
-    console.log(user);
+  const submit = async () => {
+    setErrorCode(-1);
+    setSuccessMsg("");
+    const response = await axiosRequest({
+      method: "POST",
+      baseURL: process.env.REACT_APP_BASE_APP,
+      url: "/login/reset-password",
+      data: {
+        email,
+      },
+    });
+    if (response.error && response.complete) {
+      setErrorCode(response.data.errorCode);
+    } else {
+      setSuccessMsg("Un lien pour réinitialiser le mot de passe de votre compte a été envoyé avec succès à votre addresse e-mail !")
+    }
   };
 
   const handleLinkClick = path => (event) => {
@@ -25,16 +46,30 @@ function ResetPassword(props) {
     <div className="text-center">
       <Typography color="primary" variant="h1" style={{ marginTop: "2rem" }}>Réinitialiser le mot de passe</Typography>
       <form className="login-form" noValidate autoComplete="off">
+        {(errorCode === 0) && (
+          <Typography variant="caption" color="error">
+            {errorMessages[0]}
+          </Typography>
+        )}
+
+        {(successMsg.length > 0) && (
+          <Typography variant="caption" color="primary">
+            {successMsg}
+          </Typography>
+        )}
+
         <TextField
           id="username"
           name="username"
           type="text"
           color="secondary"
           label="E-mail ou pseudo de la classe"
-          value={user.username}
+          value={email}
           onChange={handleUserNameInputChange}
           variant="outlined"
           fullWidth
+          error={errorCode === 1}
+          helperText={errorCode === 1 ? errorMessages[1] : null}
         />
 
         <Button variant="contained" color="secondary" onClick={submit}>
