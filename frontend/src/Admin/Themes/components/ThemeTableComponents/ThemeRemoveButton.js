bb8c6902484624dc207d45f47a1645b4bf270ce4
Alexandre <aflordlc@gmail.com>
2020-03-24 10:43:39 +0100

    Dry code for better reusability

--- frontend/src/Admin/Themes/components/ThemeTableComponents/ThemeRemoveButton.js
+++ frontend/src/Admin/Themes/components/ThemeTableComponents/ThemeRemoveButton.js
@@ -4,28 +4,16 @@ import PropTypes from "prop-types";
 
 import { ThemesServiceContext } from "../../../../services/ThemesService";
 import { handleRequest } from "./ThemeButtonRequests";
-import DefaultButton from "../../../components/Buttons/DefaultButton";
-import VerificationModal from "../../../components/VerificationModal";
+import DefaultDeleteButton from "../../../components/DefaultDeleteButton";
 
 function ThemeRemoveButton(props) {
   const updateThemes = useContext(ThemesServiceContext).updateThemes;
-  const [isOpen, setIsOpen] = useState(false);
 
   const [res, setRes] = useState({
     complete: false,
     error: false,
     message: ""
   });
-
-  const openVerificationModal = () => {
-    setIsOpen(true);
-    props.history.push(`/admin/themes/delete`);
-  };
-
-  const closeVerificationModal = () => {
-    setIsOpen(false);
-    props.history.push(`/admin/themes`);
-  };
 
   async function handleRemove(event) {
     event.preventDefault();
@@ -41,25 +29,20 @@ function ThemeRemoveButton(props) {
   }
 
   return (
-    <React.Fragment>
-      <DefaultButton
-        handleAction={openVerificationModal}
-        icon={props.icon}
-        res={res}
-      />
-      <VerificationModal
-        isOpen={isOpen}
-        name={props.theme.names[Object.keys(props.theme.names)[0]]}
-        handleCloseModal={closeVerificationModal}
-        handleConfirmation={handleRemove}
-        res={res}
-      />
-    </React.Fragment>
+    <DefaultDeleteButton
+      name={props.theme.names[Object.keys(props.theme.names)[0]]}
+      handleRemove={handleRemove}
+      goTo={"/admin/themes/delete"}
+      returnTo={"/admin/themes"}
+      res={res}
+      icon={props.icon}
+    />
   );
 }
 
 ThemeRemoveButton.propTypes = {
   theme: PropTypes.object.isRequired,
+  icon: PropTypes.object.isRequired,
   match: PropTypes.object.isRequired,
   location: PropTypes.object.isRequired,
   history: PropTypes.object.isRequired
