ecdea02d7649a786b77bdac1db056cd2d0921165
David Robert-Ansart <david.robertansart@gmail.com>
2020-03-26 18:59:46 +0100

    :sparkles: Pdf generation available on frontend

--- backend/src/controllers/project.ts
+++ backend/src/controllers/project.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
-import { Controller, get } from "./controller";
+import { Controller, post } from "./controller";
 import { htmlToPDF, PDF } from "../pdf";
 import { Question } from "../entities/question";
 import { Plan } from "../entities/plan";
+import { getRepository, getCustomRepository } from "typeorm";
+import { Theme } from "../entities/theme";
+import { Scenario } from "../entities/scenario";
+import { AppError, ErrorCode } from "../middlewares/handleErrors";
+import { ThemeRepository } from "../customRepositories/themeRepository";
 
 type QuestionsFromBody = Array<{ question?: string; plans?: Array<{ url?: string; description?: string }> }>;
 
@@ -11,8 +16,20 @@ export class ProjectController extends Controller {
     super("project");
   }
 
-  @get({ path: "/pdf" })
+  @post({ path: "/pdf" })
   public async getProjectPDF(req: Request, res: Response): Promise<void> {
+    const languageCode: string = req.body.languageCode || "fr";
+    const theme: Theme | undefined = await getCustomRepository(ThemeRepository).findOneWithLabels(req.body.themeId || 0);
+    const scenario: Scenario | undefined = await getRepository(Scenario).findOne({
+      where: {
+        id: req.body.scenarioId || 0,
+        languageCode,
+      },
+    });
+    if (theme === undefined || scenario === undefined) {
+      throw new AppError("Invalid data", ErrorCode.INVALID_DATA);
+    }
+
     const questions: Question[] = [];
     for (const q of (req.body.questions || []) as QuestionsFromBody) {
       const question = new Question();
@@ -28,9 +45,9 @@ export class ProjectController extends Controller {
     }
 
     const url = await htmlToPDF(PDF.PLAN_DE_TOURNAGE, {
-      themeName: req.body.themeName || "",
-      scenarioName: req.body.scenarioName || "",
-      scenarioDescription: req.body.scenarioDescription || "",
+      themeName: theme.names.fr,
+      scenarioName: scenario.name,
+      scenarioDescription: scenario.description,
       pseudo: req.user !== undefined ? req.user.pseudo : undefined,
       questions,
     });
--- frontend/src/User/Creer/Partie4/index.js
+++ frontend/src/User/Creer/Partie4/index.js
@@ -1,68 +1,135 @@
-import React, {useContext} from "react";
-import {withRouter} from "react-router";
+import React, { useState, useContext } from "react";
+import { withRouter } from "react-router";
 import PropTypes from "prop-types";
 import QRCode from "qrcode.react";
+import { getQuestions } from "../../../util/questions";
 
-import {Breadcrumbs, Hidden, Link, Typography, Button} from "@material-ui/core";
+import {
+  Breadcrumbs,
+  Hidden,
+  Link,
+  Typography,
+  Button,
+  Backdrop,
+  CircularProgress,
+  makeStyles
+} from "@material-ui/core";
 import NavigateNextIcon from "@material-ui/core/SvgIcon/SvgIcon";
-import VideocamIcon from '@material-ui/icons/Videocam';
+import VideocamIcon from "@material-ui/icons/Videocam";
 
-import {ProjectServiceContext} from "../../../services/ProjectService";
+import { ProjectServiceContext } from "../../../services/ProjectService";
 import Steps from "../../components/Steps";
 import Inverted from "../../../components/Inverted";
+import { UserServiceContext } from "../../../services/UserService";
 
+const useStyles = makeStyles(theme => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff"
+  }
+}));
 
 function Partie4(props) {
+  const classes = useStyles();
+  const { axiosLoggedRequest } = useContext(UserServiceContext);
   const { project } = useContext(ProjectServiceContext);
+  const questions = getQuestions(project);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleHome = (event) => {
+  const generatePDF = async event => {
+    event.preventDefault();
+    setIsLoading(true);
+    const response = await axiosLoggedRequest({
+      method: "POST",
+      url: "/project/pdf",
+      data: {
+        themeId: project.themeId,
+        scenarioId: project.scenarioId,
+        questions
+      }
+    });
+    setIsLoading(false);
+    if (response.complete && !response.error) {
+      window.open(`${process.env.REACT_APP_BASE_APP}/pdf/${response.data.url}`);
+    }
+  };
+
+  const handleHome = event => {
     event.preventDefault();
     props.history.push("/create");
   };
 
   return (
     <div>
-      {
-        project.themeId !== null && project.scenarioId !== null && (
-          <React.Fragment>
-            <Hidden smDown>
-              <Breadcrumbs separator={<NavigateNextIcon fontSize="small"/>} aria-label="breadcrumb">
-                <Link color="inherit" href="/create" onClick={handleHome}>
-                  Tout les thèmes
-                </Link>
-                <Typography color="textPrimary">{project.themeName}</Typography>
-              </Breadcrumbs>
-            </Hidden>
+      {project.themeId !== null && project.scenarioId !== null && (
+        <React.Fragment>
+          <Hidden smDown>
+            <Breadcrumbs
+              separator={<NavigateNextIcon fontSize="small" />}
+              aria-label="breadcrumb"
+            >
+              <Link color="inherit" href="/create" onClick={handleHome}>
+                Tout les thèmes
+              </Link>
+              <Typography color="textPrimary">{project.themeName}</Typography>
+            </Breadcrumbs>
+          </Hidden>
 
-            <Steps activeStep={3}/>
+          <Steps activeStep={3} />
 
-            <div style={{ maxWidth: "1000px", margin: "auto", paddingBottom: "2rem" }}>
-              <Typography color="primary" variant="h1">
-                <Inverted round>4</Inverted> À votre <Inverted>caméra</Inverted> !
-                <VideocamIcon fontSize="large" color="primary" style={{ transform: "translateY(0.5rem)", marginLeft: "1.5rem" }}/>
-              </Typography>
+          <div
+            style={{
+              maxWidth: "1000px",
+              margin: "auto",
+              paddingBottom: "2rem"
+            }}
+          >
+            <Typography color="primary" variant="h1">
+              <Inverted round>4</Inverted> À votre <Inverted>caméra</Inverted> !
+              <VideocamIcon
+                fontSize="large"
+                color="primary"
+                style={{
+                  transform: "translateY(0.5rem)",
+                  marginLeft: "1.5rem"
+                }}
+              />
+            </Typography>
 
-              <Typography variant="h2" style={{marginBottom: "1rem"}}>
-                Téléchargez votre plan de tournage :
-              </Typography>
-              <div className="text-center">
-                <Button className="mobile-full-width" variant="contained" color="secondary">
-                  plan de tournage
-                </Button>
-              </div>
-
-              <Typography variant="h2" style={{margin: "1rem 0"}}>
-                Flashez ce code QR pour accéder directement à l&apos;application et commencer à filmer !
-              </Typography>
-              <div className="text-center">
-                <QRCode
-                  size={192}
-                  value="https://par-le-monde-1.herokuapp.com/create/4-to-your-camera"/>
-              </div>
+            <Backdrop
+              className={classes.backdrop}
+              open={isLoading}
+              onClick={() => {}}
+            >
+              <CircularProgress color="inherit" />
+            </Backdrop>
+            <Typography variant="h2" style={{ marginBottom: "1rem" }}>
+              Téléchargez votre plan de tournage :
+            </Typography>
+            <div className="text-center">
+              <Button
+                className="mobile-full-width"
+                variant="contained"
+                color="secondary"
+                onClick={generatePDF}
+              >
+                plan de tournage
+              </Button>
             </div>
-          </React.Fragment>
-        )
-      }
+
+            <Typography variant="h2" style={{ margin: "1rem 0" }}>
+              Flashez ce code QR pour accéder directement à l&apos;application
+              et commencer à filmer !
+            </Typography>
+            <div className="text-center">
+              <QRCode
+                size={192}
+                value="https://par-le-monde-1.herokuapp.com/create/4-to-your-camera"
+              />
+            </div>
+          </div>
+        </React.Fragment>
+      )}
     </div>
   );
 }
