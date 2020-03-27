8b814421be64ebe6ebcdb9c77e1aae15cfa4b08f
Alexandre <aflordlc@gmail.com>
2020-03-27 10:24:03 +0100

    Delete user

--- frontend/src/Admin/Users/components/userRequest.js
+++ frontend/src/Admin/Users/components/userRequest.js
@@ -61,4 +61,38 @@ async function updateUser(
   return error;
 }
 
-export { updateUser };
+async function deleteUser(
+  axiosLoggedRequest,
+  user,
+  setRes,
+  successMessage,
+  errorMessage,
+  history,
+  updateUsers
+) {
+  const request = await axiosLoggedRequest({
+    method: "DELETE",
+    url: `/users/${user.id}`
+  });
+
+  if (request.error === true && request.complete === true) {
+    setRes({
+      error: true,
+      complete: true,
+      message: errorMessage
+    });
+  }
+
+  if (request.error === false && request.complete === true) {
+    setRes({
+      error: false,
+      complete: true,
+      message: successMessage
+    });
+  }
+
+  updateUsers().catch();
+  history.push("/admin/users");
+}
+
+export { updateUser, deleteUser };
