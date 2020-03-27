50c55d123b4eb42b1da3f0e95b5c28dbe89d3456
David Robert-Ansart <david.robertansart@gmail.com>
2020-03-27 14:55:10 +0100

    :sparkles: Save scenarios for non-connected users locally

--- backend/src/controllers/project.ts
+++ backend/src/controllers/project.ts
@@ -20,14 +20,23 @@ export class ProjectController extends Controller {
   public async getProjectPDF(req: Request, res: Response): Promise<void> {
     const languageCode: string = req.body.languageCode || "fr";
     const theme: Theme | undefined = await getCustomRepository(ThemeRepository).findOneWithLabels(req.body.themeId || 0);
-    const scenario: Scenario | undefined = await getRepository(Scenario).findOne({
+    let scenario: Scenario | undefined = await getRepository(Scenario).findOne({
       where: {
         id: req.body.scenarioId || 0,
         languageCode,
       },
     });
-    if (theme === undefined || scenario === undefined) {
+    if (theme === undefined) {
       throw new AppError("Invalid data", ErrorCode.INVALID_DATA);
+    }
+    if (scenario === undefined) {
+      if (req.body.scenarioName !== undefined && req.body.scenarioDescription !== undefined) {
+        scenario = new Scenario();
+        scenario.name = req.body.scenarioName;
+        scenario.description = req.body.scenarioDescription;
+      } else {
+        throw new AppError("Invalid data", ErrorCode.INVALID_DATA);
+      }
     }
 
     const questions: Question[] = [];
--- frontend/src/User/Creer/Partie1/components/ScenarioCard.js
+++ frontend/src/User/Creer/Partie1/components/ScenarioCard.js
@@ -1,42 +1,59 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
-import { ReactComponent as Arrow } from '../../../../images/right-arrow.svg';
-import {Typography, makeStyles} from "@material-ui/core";
-import {ProjectServiceContext} from "../../../../services/ProjectService";
+import { ReactComponent as Arrow } from "../../../../images/right-arrow.svg";
+import { Typography, makeStyles } from "@material-ui/core";
+import { ProjectServiceContext } from "../../../../services/ProjectService";
 
 import "./card.css";
 
 const useStyles = makeStyles(theme => ({
   greenBorder: {
     borderColor: (theme.palette.secondary || {}).main,
-    border: "1px solid",
-  },
+    border: "1px solid"
+  }
 }));
 
-function ScenarioCard({title, description, stepNumber, path, scenarioId, history, shortPath}) {
+function ScenarioCard({
+  title,
+  description,
+  stepNumber,
+  path,
+  scenarioId,
+  history,
+  shortPath
+}) {
   const classes = useStyles();
   const { updateProject } = useContext(ProjectServiceContext);
 
-  const handleClick = (event) => {
+  const handleClick = event => {
     event.preventDefault();
     updateProject({ scenarioId, scenarioName: title });
     history.push(shortPath);
   };
 
-  return <a className={[classes.greenBorder, "card-container"].join(" ")} tabIndex="0" href={path} onClick={handleClick}>
-    <div>
-      <Typography color="primary" variant="h3">{title}</Typography>
-    </div>
-    <div>
-      <p>{description}</p>
-    </div>
-    <div className="steps">
-      {stepNumber} étape{stepNumber > 1 && "s"}
-    </div>
-    <div className="arrow">
-      <Arrow />
-    </div>
-  </a>
+  return (
+    <a
+      className={[classes.greenBorder, "card-container"].join(" ")}
+      tabIndex="0"
+      href={path}
+      onClick={handleClick}
+    >
+      <div>
+        <Typography color="primary" variant="h3">
+          {title}
+        </Typography>
+      </div>
+      <div>
+        <p>{description}</p>
+      </div>
+      <div className="steps">
+        {stepNumber} étape{stepNumber > 1 && "s"}
+      </div>
+      <div className="arrow">
+        <Arrow />
+      </div>
+    </a>
+  );
 }
 
 ScenarioCard.propTypes = {
@@ -46,11 +63,11 @@ ScenarioCard.propTypes = {
   history: PropTypes.object.isRequired,
   path: PropTypes.string.isRequired,
   shortPath: PropTypes.string.isRequired,
-  scenarioId: PropTypes.number.isRequired,
+  scenarioId: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 };
 
 ScenarioCard.defaultProps = {
-  stepNumber: 0,
+  stepNumber: 0
 };
 
 export default ScenarioCard;
