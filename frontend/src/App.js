091a7bd358fbfaa631d167c21b6ba82dafde71c6
Alexandre <aflordlc@gmail.com>
2020-04-13 10:34:03 +0200

    Add React lazy to Admin page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-import Admin from "./Admin";
 import User from "./User";
 import { AppLanguageServiceProvider } from "./services/AppLanguageService";
 import { LanguagesServiceProvider } from "./services/LanguagesService";
 import { UserServiceProvider } from "./services/UserService";
 
 import theme from "./util/theme";
+
+const Admin = lazy(() => {
+  return import("./Admin");
+});
+
+const LoadingComponent = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <CircularProgress style={{ width: "300px", height: "300px" }} />
+    </div>
+  );
+};
 
 function App() {
   return (
@@ -19,10 +38,14 @@ function App() {
         <BrowserRouter>
           <UserServiceProvider>
             <LanguagesServiceProvider>
-              <Switch>
-                <Route path="/admin" component={Admin} />
-                <Route path="/" component={User} />
-              </Switch>
+              <Suspense fallback={<LoadingComponent />}>
+                <Switch>
+                  <Route path="/admin">
+                    <Admin />
+                  </Route>
+                  <Route path="/" component={User} />
+                </Switch>
+              </Suspense>
             </LanguagesServiceProvider>
           </UserServiceProvider>
         </BrowserRouter>
