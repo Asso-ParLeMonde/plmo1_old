3ad1d5884f176dbb187ae8dbc3621aa58b92721a
Alexandre <aflordlc@gmail.com>
2020-04-13 16:43:47 +0200

    Remove unecesary imports

--- frontend/src/User/index.js
+++ frontend/src/User/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Hidden from "@material-ui/core/Hidden";
 import Container from "@material-ui/core/Container";
