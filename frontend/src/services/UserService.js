8cde818a9157916c29794d0631d9d15336e0dddd
David Robert-Ansart <david.robertansart@gmail.com>
2020-04-16 22:28:42 +0200

    :rotating_light: Remove unused variable

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { withRouter } from "react-router";
 import PropTypes from "prop-types";
 import { axiosRequest } from "../components/axiosRequest";
